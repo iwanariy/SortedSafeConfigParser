ca6da950a7c0aa9a8f86fe37d8f4fbe73c962fe5
narikin <yuki.iwnr@gmail.com>
2015-02-15 22:06:53 +0900

    Copy write() from ConfigParser.py

--- SortedSafeConfigParser/SortedSafeConfigParser.py
+++ SortedSafeConfigParser/SortedSafeConfigParser.py
@@ -2,6 +2,23 @@
 
 from ConfigParser import SafeConfigParser
 
+DEFAULTSECT = "DEFAULT"
+
 
 class SortedSafeConfigParser(SafeConfigParser):
-    pass
+    def write(self, fp):
+        """Write an .ini-format representation of the configuration state."""
+        if self._defaults:
+            fp.write("[%s]\n" % DEFAULTSECT)
+            for (key, value) in self._defaults.items():
+                fp.write("%s = %s\n" % (key, str(value).replace('\n', '\n\t')))
+            fp.write("\n")
+        for section in self._sections:
+            fp.write("[%s]\n" % section)
+            for (key, value) in self._sections[section].items():
+                if key == "__name__":
+                    continue
+                if (value is not None) or (self._optcre == self.OPTCRE):
+                    key = " = ".join((key, str(value).replace('\n', '\n\t')))
+                fp.write("%s\n" % (key))
+            fp.write("\n")
