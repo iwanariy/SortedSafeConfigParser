2bcc1a0442ec70a32ee74a2d9fbcaceca87c9cfd
narikin <yuki.iwnr@gmail.com>
2015-02-16 23:53:48 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+
+from distutils.core import setup
+
+
+setup(
+    name='SortedSafeConfigParser',
+    version='0.1.0',
+    packages=['SortedSafeConfigParser'],
+    package_dir={'SortedSafeConfigParser': 'SortedSafeConfigParser'},
+)
